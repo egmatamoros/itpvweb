5cfb8410c3df0351bb4dbf12bf4e17a70c1b1123
Erick Garcia <erick.garcia.miston@gmail.com>
2022-09-09 22:49:31 -0500

    inicio

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Home',
+  components: {
+  },
+  methods: {
+
+  },
+  data: () => ({
+
+  })
+}
+</script>
+<style scoped>
+
+</style>
