a400546c99e86183ff105d793b14c2b0c148a0e2
Grecia Cardona <crimsondestiny96@gmail.com>
2022-09-10 12:17:23 -0500

    inicio de sesion be

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,56 @@
     </v-app-bar>
 
     <v-main>
-      <router-view/>
+        <v-form>
+          <v-container>
+              <v-col
+                cols="12"
+                sm="6"
+                md="3"
+              >
+                <v-text-field
+                  label="Usuario"
+                  solo
+                ></v-text-field>
+              </v-col>
+
+              <v-col
+                cols="12"
+                sm="6"
+                md="3"
+              >
+                <v-text-field
+                  label="Contraseña"
+                  solo
+                ></v-text-field>
+              </v-col>
+
+              <v-col
+                cols="12"
+                sm="6"
+                md="3"
+              >
+              <v-row align="center">
+            <v-col
+              cols="12"
+              sm="6"
+            >
+              <div class="text-center">
+      
+                <div class="my-2">
+                  <v-btn
+                    color="blue-ligth"
+                    dark
+                  >
+                    Ingresar
+                  </v-btn>
+                </div>
+              </div>
+            </v-col>
+          </v-row>
+              </v-col>
+          </v-container>
+        </v-form>
     </v-main>
   </v-app>
 </template>
